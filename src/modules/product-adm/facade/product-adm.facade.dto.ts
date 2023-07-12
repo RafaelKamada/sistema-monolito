23137116fd8fbaabbfc07c521f4be3d74c2671ae
RafaelKamada <rafael-kamada2007@hotmail.com>
2023-07-11 21:35:42 -0300

    Criando interface da Facade

---
+++ src/modules/product-adm/facade/product-adm.facade.dto.ts
@@ -0,0 +1,16 @@
+export interface AddProductFacadeInputDto {
+    id?: string;
+    name: string;
+    description: string;
+    purchasePrice: number;
+    stock: number;
+}
+
+export interface CheckProductFacadeInputDto {
+    productId: string;
+}
+
+export interface CheckProductFacadeOutputDto {
+    productId: string;
+    stock: number;
+}
---
+++ src/modules/product-adm/facade/product-adm.facade.interface.ts
@@ -0,0 +1,6 @@
+import { AddProductFacadeInputDto, CheckProductFacadeInputDto, CheckProductFacadeOutputDto } from './product-adm.facade.dto';
+
+export default interface ProductAdmFacadeInterface {
+    addProduct(input: AddProductFacadeInputDto): Promise<void>;
+    checkStock(input: CheckProductFacadeInputDto): Promise<CheckProductFacadeOutputDto>;
+}
