5e490636c1b6d4cf60ab741a75076116a65e16f4
RafaelKamada <rafael-kamada2007@hotmail.com>
2023-07-12 22:13:41 -0300

    Criando interface da StoreCatalogFacade

---
+++ src/modules/store-catalog/facade/store-catalog.facade.interface.ts
@@ -0,0 +1,6 @@
+import { FindAllStoreCatalogFacadeOutputDto, FindStoreCatalogFacadeInputDto, FindStoreCatalogFacadeOutputDto } from "./store-catalog.facade.dto";
+
+export default interface StoreCatalogFacadeInterface {
+    find(id: FindStoreCatalogFacadeInputDto): Promise<FindStoreCatalogFacadeOutputDto>;
+    findAll(): Promise<FindAllStoreCatalogFacadeOutputDto>;
+}
