c730e3122a6b6a26362e36cb620e2c38c9d966e2
RafaelKamada <rafael-kamada2007@hotmail.com>
2023-07-30 09:58:13 -0300

    Implementado api post para /clients

---
+++ src/infrastructure/api/__tests__/clients.2e2.spec.ts
@@ -0,0 +1,39 @@
+import { app, sequelize } from "../express";
+import request from "supertest";
+
+describe("E2E test for client", () => {
+  beforeEach(async () => {
+    await sequelize.sync({ force: true });
+  });
+    
+  afterAll(async () => {
+    await sequelize.close();
+  });
+  
+  it("should create a client", async () => {
+    const response = await request(app)
+      .post("/clients")
+      .send({
+        name: "Client 1",
+        email: "e@e.com",
+        document: "Document 1",
+        street: "Street 1",
+        number: "123",
+        complement: "",
+        city: "City 1",
+        state: "State 1",
+        zipCode: "12345",
+      });
+    
+    expect(response.status).toBe(200);
+    expect(response.body.name).toBe("Client 1");
+    expect(response.body.email).toBe("e@e.com");
+    expect(response.body.document).toBe("Document 1");
+    expect(response.body.street).toBe("Street 1");
+    expect(response.body.number).toBe("123");
+    expect(response.body.complement).toBe("");
+    expect(response.body.city).toBe("City 1");
+    expect(response.body.state).toBe("State 1");
+    expect(response.body.zipCode).toBe("12345");
+  });
+});
---
+++ src/infrastructure/api/routes/client.route.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from "express";
+import ClientAdmFacadeFactory from "../../../modules/client-adm/factory/client-adm.facade.factory";
+
+export const clientRoute = express.Router();
+
+clientRoute.post('/', async (req: Request, res: Response) => {
+    const facade = ClientAdmFacadeFactory.create();
+    try {
+        const clinetDto = {
+            name: req.body.name,
+            email: req.body.email,
+            document: req.body.document,
+            street: req.body.street,
+            number: req.body.number,
+            complement: req.body.complement,
+            city: req.body.city,
+            state: req.body.state,
+            zipCode: req.body.zipCode,
+        };
+        
+        const output = await facade.add(clinetDto);
+        res.send(output);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
